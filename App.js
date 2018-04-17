185dedf8a81d6a47a2972ecf2fe2e7c3b12ca967
Agnese Folli <agnesefolli@gmail.com>
2018-04-17 11:10:15 +0200

    Font async e conditional rendering TO TEST

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+
+import Font from 'expo'
 
 export default class App extends React.Component {
+  state = {
+    loading: true,
+    fontLoaded: true
+  }
+  async componentDidMount() {
+    await Font.loadAsync({
+      //bisogna scaricare il font e metterlo nell'assets
+      "pacifico-regular" : require('./assets/fonts/Pacifico-Regular.ttf')
+    })
+    this.setState({fontLoaded: true});
+    //setTimeout(()=> this.setState({loading: false}), 2000);
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        {this.state.fontLoaded ? (
+          <ActivityIndicator size="large" color="blue"/>
+        ) : (
+            <Text style={{fontFamily: 'pacifico-regular'}}>Open up App.js to start working on your app!</Text>
+        )}
       </View>
     );
   }
