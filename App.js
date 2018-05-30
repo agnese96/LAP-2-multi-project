5170e442a81910a7c5fc28180c9e8ab392338722
Agnese Folli <agnesefolli@gmail.com>
2018-05-30 14:40:32 +0200

    Fix paths and ignore

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { createStackNavigator } from 'react-navigation';
-import todolistReducer from './reducers';
+import todolistReducer from './src/reducers';
 import { Provider, connect } from 'react-redux';
 import { createStore } from 'redux';
 
 const store = createStore(todolistReducer);
 
 // devo prendere il container solo per gli screen che utilizzano lo store.
-import ToDoList from './containers/ToDoList'
-import AddToDo from './components/AddToDo'
-import EditToDo from './components/EditToDo'
+import ToDoList from './src/containers/ToDoList'
+import AddToDo from './src/components/AddToDo'
+import EditToDo from './src/components/EditToDo'
 
 const RootStack = createStackNavigator(
   {
